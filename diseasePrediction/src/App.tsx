8c964a9cabf9af2d3324484a90820f0e89c1a2fb
Siddeshwari Veeramalla <sidvsid11@gmail.com>
2024-12-24 19:25:39 +0530

    Add files via upload

---
+++ diseasePrediction/src/App.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+// Lazy load the Home component
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Diseaseprediction = lazy(() => import("./pages/Diseaseprediction"));
+import LoadingPage from "./pages/LodingPage";
+
+const App: React.FC = () => {
+  useEffect(() => {
+    const cursal = document.querySelector(".cursal");
+
+    const handleMouseMove = (e: MouseEvent) => {
+      cursal?.setAttribute("style", `top: ${e.pageY - 15}px; left: ${e.pageX - 15}px;`);
+    };
+
+    const handleClick = () => {
+      cursal?.classList.add("repelAnimation");
+
+      setTimeout(() => {
+        cursal?.classList.remove("repelAnimation");
+      }, 500);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("click", handleClick);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="cursal"></div>
+      <Router>
+        <Suspense fallback={<div><LoadingPage /></div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/diseaseprediction" element={<Diseaseprediction />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </>
+  );
+};
+
+export default App;
